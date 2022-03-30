1567fac8d4a9efaf4cb850c530b704da14c998b4
JorgeIsur <ramirezisur@gmail.com>
2022-03-29 20:15:30 -0600

    Agregado actualizacion de inventario ante prestamo

--- src/models/Prestamos.js
+++ src/models/Prestamos.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+
+const inventario = require('../models/Inventario');
+
 const { Schema } = mongoose;
 
 const prestamo = new Date();
@@ -14,5 +17,21 @@ const SchemaPrestamo = new Schema({
     fecha:{type:Date, default:prestamo},
     entrega:{type:Date,default:entrega}
 });
+
+SchemaPrestamo.methods.actualizaInventario = async (articulo)=>{
+    const [{ cantidad, _id }] = await inventario.find({name:articulo}).lean();
+    const valor = parseInt(cantidad)-1;
+    console.log(valor);
+    console.log(_id.toString());
+    await inventario.findByIdAndUpdate(_id.toString(),{cantidad:valor.toString()});
+};
+
+SchemaPrestamo.methods.regresaArticulo = async (articulo)=>{
+    const [{ cantidad, _id }] = await inventario.find({name:articulo}).lean();
+    const valor = parseInt(cantidad)+1;
+    console.log(valor);
+    console.log(_id.toString());
+    await inventario.findByIdAndUpdate(_id.toString(),{cantidad:valor.toString()});
+};
 
 module.exports = mongoose.model('Prestamos',SchemaPrestamo);
--- src/routes/misPrestamos.js
+++ src/routes/misPrestamos.js
@@ -25,7 +25,7 @@ router.post('/prestamos/nuevo-prestamo/:id',isAuthenticated,async(req,res)=>{
     const [{matricula}] = usuario;
     const cantidad = '1';
     const nuevoPrestamo = new Prestamos({name,matricula,articulo,laboratorio,cantidad});
-    console.log(req.user.id);
+    nuevoPrestamo.actualizar = await nuevoPrestamo.actualizaInventario(name);
     nuevoPrestamo.user = req.user.id;
     await nuevoPrestamo.save();
     req.flash('success_msg','Prestamo registrado');
@@ -33,26 +33,19 @@ router.post('/prestamos/nuevo-prestamo/:id',isAuthenticated,async(req,res)=>{
 });
 
 router.get('/prestamos',isAuthenticated,async(req,res)=>{
-    const prestamos = await Prestamos.find({user:req.user.id}).lean();
+    const [{matricula}] = await Users.find({name:req.user.name}).lean();
+    const prestamos = await Prestamos.find({matricula:matricula}).lean();
     res.render('prestamos/all-prestamos',{prestamos});
 });
 
-router.get('/prestamos/edit/:id',isAuthenticated, async(req,res)=>{
-    const inventario = await Inventario.findById(req.params.id).lean();
-    res.render('prestamos/all-prestamos',{inventario});
-});
-
-router.put('/prestamos/edit/:id',isAuthenticated,async(req,res)=>{
-    const {name, description, area} = req.body;
-    await Inventario.findByIdAndUpdate(req.params.id,{name,description,area});
-    req.flash('success_msg','Item editado satisfactoriamente.');
-    res.redirect('/administrar');
-});
-
 router.delete('/prestamos/delete/:id',isAuthenticated,async(req,res)=>{
-    await Inventario.findByIdAndDelete(req.params.id);
+    const [{matricula}] = await Users.find({name:req.user.name}).lean();
+    const [{name}] = await Prestamos.find({matricula:matricula}).lean();
+    const regresoPrestamo = new Prestamos();
+    regresoPrestamo.actualizar = await regresoPrestamo.regresaArticulo(name);
+    await Prestamos.findByIdAndDelete(req.params.id);
     req.flash('success_msg','Prestamo cancelado satisfactoriamente.');
-    res.redirect('/administrar');
+    res.redirect('/prestamos');
 });
 
 module.exports = router;
